192f71c7fedb90a7bdc8589a0b0d885e455fb692
James Graca-Jones <jamesgracajones@gmail.com>
2022-01-17 22:17:45 +0000

    Implement a way to save the best trained model.

--- cnns/training.py
+++ cnns/training.py
@@ -136,7 +136,7 @@ print(f"After model instantiation: {torch.cuda.memory_allocated(0)}")
 sys.path.insert(1, "/home/james/VSCode/DataLoading")
 from DataLoader2 import RonchigramDataset
 
-ronchdset = RonchigramDataset("/media/rob/hdd2/simulations/17_01_22/james/Single_Aberrations.h5")
+ronchdset = RonchigramDataset("/media/rob/hdd2/james/simulations/17_01_22/Single_Aberrations.h5")
 
 print(f"After ronchdset instantiation: {torch.cuda.memory_allocated(0)}")
 
@@ -355,7 +355,6 @@ tb_logger = TensorboardLogger(log_dir=log_path)
 tb_logger.attach(trainer, log_handler=OutputHandler('training', ['batchloss', ]), event_name=Events.ITERATION_COMPLETED)
 print("Experiment name: ", exp_name)
 
-
 # Learning rate scheduling
 trainer.add_event_handler(Events.EPOCH_COMPLETED, lambda engine: lr_scheduler.step())
 
@@ -481,7 +480,7 @@ testEvaluator.add_event_handler(Events.COMPLETED, empty_cuda_cache)
 
 # ACTUAL TRAINING
 
-num_epochs = 20
+num_epochs = 1
 
 # This is where training begins
 # Note: when training, in the display for a given epoch, while the epoch is running, x/y shows the number of batches 
@@ -501,6 +500,57 @@ print(f"testEvaluator metrics: {testEvaluator.state.metrics}")
 
 
 
-# STORING THE BEST MODEL FOR TRAINING
+# STORING THE BEST MODEL FROM TRAINING
 
-# Add loss curves
+os.system(f"ls {log_path}")
+
+# NOTE: out why print(next(os.walk(log_path)))[1] returns just an empty list - this 
+# is because the ".pt" files really are just files, not directories (although their 
+# icons make them look like directories). I think they are zip files.
+
+# Takes all files in the directory log_path and its sub-directories
+checkpoints = next(os.walk(log_path))[2]
+print("\n" + str(checkpoints))
+
+# Keeps only the files ending in ".pt"
+checkpoints = sorted(filter(lambda f: f.endswith(".pt"), checkpoints))
+print("\n" + str(checkpoints))
+
+# TODO: automate calculation of the indices of the file names to take the 
+# scores from below
+scores = [c[32:40] for c in checkpoints]
+print("\nScores:", scores)
+
+# TODO: check the following--I think bestEpoch is a misnomer, but for 
+# some reason I have kept it called that like the Kaggle webpage I first 
+# got this training pipeline from does. I think it actually just refers 
+# to the best model but I could be wrong.
+bestEpoch = np.argmax(scores)
+print("\n" + str(bestEpoch))
+
+if not checkpoints:
+    print(f"\nThere are no weight files in {log_path}")
+
+else:
+    modelPath = f"{log_path}/efficientNetBestReciprocalMSE_{scores[bestEpoch]}"
+    print("\nNew best model weights path:", modelPath)
+
+    currentBestModelPath = os.path.join(log_path, checkpoints[bestEpoch])
+    print("\n" + currentBestModelPath)
+
+    os.system(f"cp -r {currentBestModelPath} {modelPath}")
+
+removeOtherFiles = False
+
+if removeOtherFiles:
+    # Rather than remove everything in log_path, just want to remove the files 
+    # that aren't the very best model weights. Don't want to remove everything 
+    # in log_path because the very best model weights are stored there, so want 
+    # to keep that.
+    # TODO: as they arise, add other non-essential files to automatically delete, 
+    # but not the loss curves, of course.
+    os.system(f"rm {log_path}/best_model_test* {log_path}/events*")
+
+
+
+# TODO: add loss curves
