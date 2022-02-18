e922a340c2d1bf3be255864156d39fb2c4fe3d31
James Graca-Jones <jamesgracajones@gmail.com>
2022-02-18 13:51:28 +0000

    Add clipping to 10**9 to the def of myMAPE to mimic pytorch-forecast's MAPE.

---
+++ currentPipelines/customLossFuncs.py
@@ -0,0 +1,42 @@
+from pytorch_forecasting.metrics import MultiHorizonMetric
+import torch
+import sys
+
+class modifiedMAPE(MultiHorizonMetric):
+    """
+    Mean absolute percentage error (in %, unlike pytorch_forecasting.metrics.MAPE where a fraction is computed instead). 
+    Increments elements of y (label) equal to zero by a chosen amount, unlike for pytorch_forecasting.metrics.MAPE, 
+    where every y is incremented by 1e-8.
+
+    Defined as ``(y - y_pred).abs() / y.abs()`` for elements of y that are nonzero, but the denominator is augmented 
+    for elemnents of y that are zero.
+    """
+
+    def loss(self, y_pred, target):
+        # TODO: make epsilon (10**-13) an array that has different values for each element, according to what makes an element 
+        # negligible (see Google doc 16/02/22)
+
+        # NOTE: divided by 10**7 because this led to modifiedMAPE's handler to not have to clip the loss value to prevent
+        # gradients that were too large, at least it seemed to work at 12pm on 18/02/22. If y_pred labels are made 
+        # larger, however, it may be that the 10**7 is no longer sufficient.
+        loss = (self.to_prediction(y_pred) - target).abs() / (target.abs() + 1e-13) / 10**7
+
+        return loss
+
+def myMAPE(output, target):
+    """
+    Mean absolute percentage error (in %, unlike pytorch_forecasting.metrics.MAPE where a fraction is computed instead). 
+    Increments elements of y (label) equal to zero by a chosen amount, unlike for pytorch_forecasting.metrics.MAPE, 
+    where every y is incremented by 1e-8.
+
+    Defined as ``(y - y_pred).abs() / y.abs()`` for elements of y that are nonzero, but the denominator is augmented 
+    for elemnents of y that are zero.
+    """
+    # TODO: make epsilon (10**-13) an array that has different values for each element, according to what makes an element 
+    # negligible (see Google doc 16/02/22)
+    loss = torch.mean((output - target).abs() / (target.abs() + 10**-13) * 100)
+
+    if loss > 10**9:
+        loss[0] = 10**9
+
+    return loss
