883ca7647282620c1b1f7093841261120061b82d
James Graca-Jones <jamesgracajones@gmail.com>
2022-02-19 11:08:22 +0000

    Minor modifications.

--- currentPipelines/customLossFuncs.py
+++ currentPipelines/customLossFuncs.py
@@ -31,12 +31,40 @@ def myMAFE(output, target):
 
     Defined as ``(y - y_pred).abs() / y.abs()`` for elements of y that are nonzero, but the denominator is augmented 
     for elemnents of y that are zero.
-
-    Given the lack of multiplication by 100, this is actually 
     """
+
     # TODO: make epsilon (10**-13) an array that has different values for each element, according to what makes an element 
     # negligible (see Google doc 16/02/22)
     loss = torch.mean((output - target).abs() / (target.abs() + 10**-13))
+
+    # NOTE: because the loss is on the CPU (I think) it is hard to simpyl use the if statement below to clip losses 
+    # greater than 10**9 to 10**9, so I had to use the below method.
+    a = loss < 10**9
+    b = loss >= 10**9
+
+    loss = loss * a + b * 10**9
+
+    # if loss.size() != torch.Size([]):
+    #     if loss > 10**9:
+    #         loss[0] = 10**9
+
+    return loss
+
+def myMAFE2(output, target):
+    """
+    Mean absolute fraction error. 
+    Increments elements of y (label) equal to zero by a chosen amount, unlike for pytorch_forecasting.metrics.MAPE, 
+    where every y is incremented by 1e-8.
+
+    Aberration angles associated with magnitudes that are negligible are discarded from mean calculation.
+
+    Defined as ``(y - y_pred).abs() / y.abs()`` for elements of y that are nonzero, but the denominator is augmented 
+    for elemnents of y that are zero.
+    """
+
+    # TODO: make epsilon (10**-13) an array that has different values for each element, according to what makes an element 
+    # negligible (see Google doc 16/02/22)
+    loss = (output - target).abs() / (target.abs() + 10**-13)
 
     # NOTE: because the loss is on the CPU (I think) it is hard to simpyl use the if statement below to clip losses 
     # greater than 10**9 to 10**9, so I had to use the below method.
