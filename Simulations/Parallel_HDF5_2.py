7555a6d83e99920b7e477459a91762851600a0d4
James Graca-Jones <jamesgracajones@gmail.com>
2022-02-27 21:00:10 +0000

    Set up for simulations of Ronchigrams with linear C23 but random other values.

--- Simulations/Parallel_HDF5_2.py
+++ Simulations/Parallel_HDF5_2.py
@@ -95,8 +95,8 @@ if __name__ == "__main__":
 
                 C10 = randu(0, max_C10)
                 C12 = randu(0, max_C12)
-                C21 = linearC21[simulation]
-                C23 = randu(0, max_C23)
+                C21 = randu(0, max_C21)
+                C23 = linearC23[simulation]
 
                 # if simulation_number <= math.ceil(number_simulations / 4):
                 #     C10 = randu(0, max_C10)
