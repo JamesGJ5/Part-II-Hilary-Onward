eb104f6de2ae3bfcaaf5c699059296f013385cc8
James Graca-Jones <jamesgracajones@gmail.com>
2022-03-07 20:47:34 +0000

    Set up simulating file to create 100,000 Ronchigrams whose c12, c21 and c23 maxima are halved compared to before, and prepare to pick values randomly from ranges 0 to max_cnm (and from the typical phinm ranges except with phi10 = 0).

--- Simulations/Parallel_HDF5_2.py
+++ Simulations/Parallel_HDF5_2.py
@@ -25,9 +25,9 @@ if __name__ == "__main__":
 
     # The maxima below apply when making Ronchigrams in which the aberration in question is to be significant
     max_C10 = 100 * 10**-9  # Maximum C10 (defocus) magnitude/m
-    max_C12 = 100 * 10**-9  # Maximum C12 (2-fold astigmatism) magnitude/m
-    max_C21 = 10000 * 10**-9  # Maximum C21 (axial coma) magnitude/m
-    max_C23 = 10000 * 10**-9  # Maximum C23 (3-fold astigmatism) magnitude/m
+    max_C12 = 50 * 10**-9  # Maximum C12 (2-fold astigmatism) magnitude/m
+    max_C21 = 5000 * 10**-9  # Maximum C21 (axial coma) magnitude/m
+    max_C23 = 5000 * 10**-9  # Maximum C23 (3-fold astigmatism) magnitude/m
 
     # min_Cnm will be 0 since negative values are redundant, I THINK (see lab book's 29/11/2021 entry)
     # phi_n,m will be between 0 and pi/m radians since, I believe, other angles are redundant (see lab book's 29/11/2021 entry)
@@ -58,7 +58,7 @@ if __name__ == "__main__":
         :param max_C23: max. 3-fold astigmatism/m
         """
 
-        with h5py.File(f"/media/rob/hdd1/james-gj/Simulations/forInference/04_03_22/c10_0_to_75nm_c12_10nm_c21_1000nm_c23_1000nm.h5", "w", driver="mpio", comm=MPI.COMM_WORLD) as f:
+        with h5py.File(f"/media/rob/hdd1/james-gj/Simulations/forTraining/06_03_22/mixedAberrations.h5", "w", driver="mpio", comm=MPI.COMM_WORLD) as f:
             # Be wary that you are in write mode
 
             # TODO: code in a way to add the value(s) of b to the HDF5 file if you choose to
@@ -83,7 +83,7 @@ if __name__ == "__main__":
             # NOTE: The below variable is only useful for certain statements below
             # simulation_number = 0
 
-            linearC10 = np.linspace(rank / number_processes * max_C10, (rank + 1) / number_processes * max_C10, number_simulations, endpoint=False)
+            # linearC10 = np.linspace(rank / number_processes * max_C10, (rank + 1) / number_processes * max_C10, number_simulations, endpoint=False)
             # linearC12 = np.linspace(rank / number_processes * max_C12, (rank + 1) / number_processes * max_C12, number_simulations, endpoint=False)
             # linearC21 = np.linspace(rank / number_processes * max_C21, (rank + 1) / number_processes * max_C21, number_simulations, endpoint=False)
             # linearC23 = np.linspace(rank / number_processes * max_C23, (rank + 1) / number_processes * max_C23, number_simulations, endpoint=False)
@@ -98,15 +98,20 @@ if __name__ == "__main__":
                 # C21 = randu(0, max_C21)
                 # C23 = linearC23[simulation]
 
-                C10 = linearC10[simulation]
-                C12 = 10 * 10**-9
-                C21 = 1000 * 10**-9
-                C23 = 1000 * 10**-9
+                C10 = randu(0, max_C10)
+                C12 = randu(0, max_C12)
+                C21 = randu(0, max_C21)
+                C23 = randu(0, max_C23)
 
-                phi10 = 0
-                phi12 = 0
-                phi21 = 0
-                phi23 = 0
+                # C10 = linearC10[simulation]
+                # C12 = 10 * 10**-9
+                # C21 = 1000 * 10**-9
+                # C23 = 1000 * 10**-9
+
+                # phi10 = 0
+                # phi12 = 0
+                # phi21 = 0
+                # phi23 = 0
 
                 # if simulation_number <= math.ceil(number_simulations / 4):
                 #     C10 = randu(0, max_C10)
@@ -136,11 +141,11 @@ if __name__ == "__main__":
                 #     C12 = randu(0, C23/1000)
                 #     C21 = randu(0, C23/100)
 
-                # # Below, the ranges for 
-                # phi10 = 0   # Defocus has an m-value of 0
-                # phi12 = randu(0, np.pi/2)
-                # phi21 = randu(0, np.pi)
-                # phi23 = randu(0, np.pi/3)
+                # Below, the ranges for 
+                phi10 = 0   # Defocus has an m-value of 0
+                phi12 = randu(0, np.pi/2)
+                phi21 = randu(0, np.pi)
+                phi23 = randu(0, np.pi/3)
 
                 I = randu(min_I, max_I)
                 t = randu(min_t, max_t)
@@ -190,7 +195,7 @@ if __name__ == "__main__":
     # sys.exit()
 
     # CPUs AND PROCESSES
-    total_simulations = 4
+    total_simulations = 100000
 
     number_processes = MPI.COMM_WORLD.size
     simulations_per_process = int(math.ceil(total_simulations / number_processes))
