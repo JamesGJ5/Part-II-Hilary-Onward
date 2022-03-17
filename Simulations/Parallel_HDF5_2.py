d0caec15416fca7b45576f50c98284779939fbb7
James Graca-Jones <jamesgracajones@gmail.com>
2022-03-17 20:37:33 +0000

    Put endpoint back to False, as is necessary to prevent duplicate linear cnm values.

--- Simulations/Parallel_HDF5_2.py
+++ Simulations/Parallel_HDF5_2.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
         :param max_C23: max. 3-fold astigmatism/m
         """
 
-        with h5py.File(f"/media/rob/hdd1/james-gj/Simulations/forInference/17_03_22/linear_c10", "w", driver="mpio", comm=MPI.COMM_WORLD) as f:
+        with h5py.File(f"/media/rob/hdd1/james-gj/Simulations/forInference/17_03_22/linear_c12", "w", driver="mpio", comm=MPI.COMM_WORLD) as f:
             # Be wary that you are in write mode
 
             # TODO: code in a way to add the value(s) of b to the HDF5 file if you choose to
@@ -83,17 +83,17 @@ if __name__ == "__main__":
             # NOTE: The below variable is only useful for certain statements below
             # simulation_number = 0
 
-            linearC10 = np.linspace(rank / number_processes * max_C10, (rank + 1) / number_processes * max_C10, number_simulations, endpoint=True)
-            # linearC12 = np.linspace(rank / number_processes * max_C12, (rank + 1) / number_processes * max_C12, number_simulations, endpoint=False)
-            # linearC21 = np.linspace(rank / number_processes * max_C21, (rank + 1) / number_processes * max_C21, number_simulations, endpoint=False)
-            # linearC23 = np.linspace(rank / number_processes * max_C23, (rank + 1) / number_processes * max_C23, number_simulations, endpoint=False)
+            linearC10 = np.linspace(rank / number_processes * max_C10, (rank + 1) / number_processes * max_C10, number_simulations, endpoint=False)
+            linearC12 = np.linspace(rank / number_processes * max_C12, (rank + 1) / number_processes * max_C12, number_simulations, endpoint=False)
+            linearC21 = np.linspace(rank / number_processes * max_C21, (rank + 1) / number_processes * max_C21, number_simulations, endpoint=False)
+            linearC23 = np.linspace(rank / number_processes * max_C23, (rank + 1) / number_processes * max_C23, number_simulations, endpoint=False)
 
             # See Google doc 4th Year > 16/02/22 for how the below ranges were chosen
             for simulation in range(number_simulations):
                 # NOTE: The below variable is only useful for certain statements below
                 # simulation_number += 1
 
-                C10 = linearC10[simulation]
+                C10 = linearC10
                 C12 = randu(0, max_C12)
                 C21 = randu(0, max_C21)
                 C23 = randu(0, max_C23)
