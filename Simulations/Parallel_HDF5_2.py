682daaa19ce2c35fd701bab502d6eae0247e3258
James Graca-Jones <jamesgracajones@gmail.com>
2022-03-28 12:58:38 +0100

    Minor modifications.

--- Simulations/Parallel_HDF5_2.py
+++ Simulations/Parallel_HDF5_2.py
@@ -57,8 +57,8 @@ if __name__ == "__main__":
         :param max_C21: max. axial coma/m
         :param max_C23: max. 3-fold astigmatism/m
         """
-
-        with h5py.File(f"/media/rob/hdd1/james-gj/Simulations/forTraining/22_03_22/abersWithC30.h5", "w", driver="mpio", comm=MPI.COMM_WORLD) as f:
+        
+        with h5py.File(f"/media/rob/hdd1/james-gj/Simulations/forTraining/28_03_22/abersWithC30.h5", "w", driver="mpio", comm=MPI.COMM_WORLD) as f:
             # Be wary that you are in write mode
 
             # TODO: code in a way to add the value(s) of b to the HDF5 file if you choose to
