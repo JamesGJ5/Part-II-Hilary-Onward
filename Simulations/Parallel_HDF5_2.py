8cee90299b75953d30b535db74185994736fbe2a
James Graca-Jones <jamesgracajones@gmail.com>
2022-05-03 10:27:36 +0100

    Set up simulation of 85,000 Ronchigrams featuring only C21.

--- Simulations/Parallel_HDF5_2.py
+++ Simulations/Parallel_HDF5_2.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
         :param max_C23: max. 3-fold astigmatism/m
         """
         
-        with h5py.File(f"/media/rob/hdd1/james-gj/Simulations/forTraining/02_05_22/c12.h5", "w", driver="mpio", comm=MPI.COMM_WORLD) as f:
+        with h5py.File(f"/media/rob/hdd1/james-gj/Simulations/forTraining/02_05_22/c21.h5", "w", driver="mpio", comm=MPI.COMM_WORLD) as f:
             # Be wary that you are in write mode
 
             # TODO: code in a way to add the value(s) of b to the HDF5 file if you choose to
@@ -123,12 +123,12 @@ if __name__ == "__main__":
                 # C10 = randu(0, max_C10)
                 # C10 = max_C10 / 2
 
-                # C12 = 0
-                C12 = randu(0, max_C12)
+                C12 = 0
+                # C12 = randu(0, max_C12)
                 # C12 = linearC12[simulation]
 
-                C21 = 0
-                # C21 = randu(0, max_C21)
+                # C21 = 0
+                C21 = randu(0, max_C21)
                 # C21 = max_C21 / 2
 
                 C23 = 0
@@ -178,12 +178,12 @@ if __name__ == "__main__":
 
                 phi10 = 0
 
-                # phi12 = 0
-                phi12 = randu(0, 2 * np.pi / 2)
+                phi12 = 0
+                # phi12 = randu(0, 2 * np.pi / 2)
                 # phi12 = linearPhi12[simulation]
 
-                phi21 = 0
-                # phi21 = randu(0, 2 * np.pi / 1)
+                # phi21 = 0
+                phi21 = randu(0, 2 * np.pi / 1)
                 # phi21 = 2 * np.pi / 2
 
                 phi23 = 0
