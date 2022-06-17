Set up the depiction of a simulation in which the average values for simulations made in this project to emulate the C3,0-corrected STEM are generated, with a detector semi-angle of 180mrad.

Simulations/Primary_Simulation_1.py
@@ -574,7 +574,7 @@ if __name__ == "__main__":
     # print(ang_list[1] / (2 * np.pi / 2))
     # print(mag_list[1] / (2 * 50 * 10**-9))
 
-    imdim = 2672
+    imdim = 1024
 
     useZhiyuanParams = False
 